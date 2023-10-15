2e920121e90d30ac8ec86544c8160551b2495946
NFGase <776298399@qq.com>
2023-10-15 14:29:19 +0800

    快读快写升级
    快读快写可以支持多个读入了！

--- Template/快读快写.cpp
+++ Template/快读快写.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 namespace io{
-    inline long long read(){long long x = 0, f = 1; char ch = getchar(); while(ch < '0' || ch > '9'){if(ch == '-') f = -1; ch = getchar();} while(ch >= '0' && ch <= '9') x = x * 10 + ch - '0', ch = getchar(); return x * f;}
-    inline void write(long long x){if(x < 0) putchar('-'), x = -x; if(x > 9) write(x / 10); putchar(x % 10 + '0'); return ;}
+    template <typename T> inline void read(T& x){x = 0; bool f = false; char ch = getchar(); while(ch < '0' || ch > '9'){if(ch == '-') f = !f; ch = getchar();}while(ch >= '0' && ch <= '9'){x = (x << 3) + (x << 1) + (ch ^ 48); ch = getchar();} x = (f ? -x : x); return;}
+    template <typename T, typename... Args> inline void read(T& x, Args&...x_){read(x), read(x_...);}
+    template <typename T> inline void put(T x){if(x < 0) putchar('-'), x = -x; if(x > 9) put(x / 10); putchar(x % 10 + '0'); return ;}
+    template <typename T> inline void write(T x){put(x);}
+    template <typename T, typename... Args> inline void write(T x, Args...x_){write(x), write(x_...);}
 }
 long long a, b;
 int main(){
-    a = io::read(), b = io::read();
+    io::read(a, b);
     io::write(a + b);
     return 0;
 }
