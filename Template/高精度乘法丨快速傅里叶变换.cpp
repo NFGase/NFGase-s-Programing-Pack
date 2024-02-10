f313f5a0e60a76fe12118eb76ed770ba32ce412c
NFGase <NFGase_New@outlook.com>
2024-02-10 15:22:17 +0800

    更改名称。

--- Template/高精度乘法丨快速傅里叶变换.cpp
+++ Template/高精度乘法丨快速傅里叶变换.cpp
@@ -1,6 +1,7 @@
-// https://www.luogu.com.cn/problem/P3803
+// https://www.luogu.com.cn/problem/P1919
 #include <iostream>
 #include <complex>
+#include <string>
 namespace io{
     template <typename T> inline void read(T& x){x = 0; bool f = false; char ch = getchar(); while(ch < '0' || ch > '9'){if(ch == '-') f = !f; ch = getchar();}while(ch >= '0' && ch <= '9'){x = (x << 3) + (x << 1) + (ch ^ 48); ch = getchar();} x = (f ? -x : x); return;}
     template <typename T, typename... Args> inline void read(T& x, Args&...x_){read(x), read(x_...);}
@@ -10,8 +11,9 @@ namespace io{
     inline void newb(){putchar(' ');}
 }
 namespace code{
-    int n, m, lim, r[10000001], cnt = 0;
     const double pi = acos(-1);
+    int n, m, lim, r[10000001], cnt = 0, rev[10000001], res[10000001], bit, tot, top, l = 0;
+    std::string s1, s2;
     std::complex <double> a[10000001], b[10000001];
     void FastFourierTransform(std::complex <double> * a, int option){
         for(int i = 0; i < lim; i++) if(i < r[i]) swap(a[i], a[r[i]]);
@@ -31,15 +33,27 @@ namespace code{
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         std::cout.tie(nullptr);
-        std::cin >> n >> m;
-        for(int i = 0; i <= n; i++) std::cin >> a[i];
-        for(int i = 0; i <= m; i++) std::cin >> b[i];
+        std::cin >> s1 >> s2;
+        n = s1.length() - 1, m = s2.length() - 1;
+        for(int i = 0; i <= n; i++) a[i] = s1[n - i] - '0';
+        for(int i = 0; i <= m; i++) b[i] = s2[m - i] - '0';
         for(lim = 1; lim <= (n + m); lim <<= 1) cnt++;
         for(int i = 0; i < lim; i++) r[i] = (r[i >> 1] >> 1) | ((i & 1) << (cnt - 1));
         FastFourierTransform(a, 1), FastFourierTransform(b, 1);
         for(int i = 0; i <= lim; i++) a[i] *= b[i];
         FastFourierTransform(a, -1);
-        for(int i = 0; i <= (n + m); i++) std::cout << (int)(0.5 + a[i].real() / lim) << " ";
+        for(int i = 0; i <= (n + m); i++) res[i] = (int)(0.5 + a[i].real() / lim);
+        top = n + m;
+        while(l <= top){
+            if(res[l] >= 10){
+                res[l + 1] += res[l] / 10;
+                top += l == top;
+                res[l] %= 10;
+            }
+            l++;
+        }
+        while(res[top] == 0 && top > 1) top--;
+        for(int i = top; i >= 0; i--) std::cout << res[i];
         return 0;
     }
 }
